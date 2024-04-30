a62fd90b60a7504f7e7a002a997de12f8a287265
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-30 19:55:36 +0000

    movies forms are working

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/insert_movie_record", { :controller => "movies", :action => "create" })
+  get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy" })
+  post("/modify_movie_record/:the_id", { :controller => "movies", :action => "update" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
